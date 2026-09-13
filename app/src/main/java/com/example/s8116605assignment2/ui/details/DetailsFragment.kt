394b1dbe5f7de9599d4850bd7f4f896c89c9ecc0
louis10houghton <houghtonlouis208@gmail.com>
2026-09-13 13:30:27 +1000

    Added details screen and removed redundant login authentication that was generated by the login class

--- app/src/main/java/com/example/s8116605assignment2/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/s8116605assignment2/ui/details/DetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.s8116605assignment2.databinding.FragmentDetailsBinding
+import com.example.s8116605assignment2.network.model.Entity
 
 class DetailsFragment : Fragment() {
 
@@ -20,6 +21,24 @@ class DetailsFragment : Fragment() {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        @Suppress("DEPRECATION")
+        val entity = arguments?.getSerializable("entity") as? Entity
+
+        entity?.let {
+            binding.detailsTechnique.text = it.technique
+            binding.detailsSubject.text = "Subject: ${it.subject}"
+            binding.detailsEquipment.text = "Equipment: ${it.equipment}"
+            binding.detailsPhotographer.text = "Pioneering Photographer: ${it.pioneeringPhotographer}"
+            binding.detailsYear.text = "Year Introduced: ${it.yearIntroduced}"
+            binding.detailsDescription.text = it.description
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
